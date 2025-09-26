f18c46eaaf75392e044a6ee0fce3a71b0a7a256c
Cislove <vim152184@gmail.com>
2025-09-27 00:45:26 +0300

    init

---
+++ lib/fp_lab1.ex
@@ -0,0 +1,18 @@
+defmodule FpLab1 do
+  @moduledoc """
+  Documentation for `FpLab1`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FpLab1.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fp_lab1_test.exs
@@ -0,0 +1,8 @@
+defmodule FpLab1Test do
+  use ExUnit.Case
+  doctest FpLab1
+
+  test "greets the world" do
+    assert FpLab1.hello() == :world
+  end
+end
